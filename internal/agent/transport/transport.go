c617fa365672c04982e56fd4e38cee0f0cebc078
artembychkov <mai.lhome@ya.ru>
2022-04-07 00:00:22 +0300

    fix go vet

--- internal/agent/transport/transport.go
+++ internal/agent/transport/transport.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"github.com/ChipArtem/Metric/internal/models"
-	"github.com/go-resty/resty/v2"
 )
 
 type HTTPclient struct {
@@ -21,17 +20,13 @@ func NewHTTPClient(url string, client *http.Client) HTTPclient {
 
 func (c HTTPclient) SendMetric(m models.Metric) error {
 	url := c.hostURL + "/update/" + m.Type + "/" + m.Name + "/" + m.Value
-	// response, err := c.client.Post(url, "text/plain", nil)
-	r, err := resty.New().R().
-		SetBody([]byte("")).
-		SetHeader("Content-Type", "text/plain").Post(url)
+	r, err := c.client.Post(url, "text/plain", nil)
 	if err != nil {
-		panic("url_" + url + "__ __" + err.Error() + "_")
 		return err
 	}
-	if r.StatusCode() != http.StatusOK {
+	if r.StatusCode != http.StatusOK {
 		return fmt.Errorf("SendMetric status %v", http.StatusOK)
 	}
-	// response.Body.Close()
+	r.Body.Close()
 	return nil
 }
