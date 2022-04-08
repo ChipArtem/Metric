0aa4bea124a13877868a6e1d934c6df3023b37f4
artembychkov <mai.lhome@ya.ru>
2022-04-09 01:08:24 +0300

    add Fprint

--- internal/agent/transport/transport.go
+++ internal/agent/transport/transport.go
@@ -22,14 +22,14 @@ func NewHTTPClient(url string, client *http.Client) HTTPclient {
 
 func (c HTTPclient) SendMetric(m models.Metric) error {
 	url := c.hostURL + "/update/" + m.Type + "/" + m.Name + "/" + m.Value
-	fmt.Fprintf(os.Stdout, "transport url ", url, "\n")
+	fmt.Fprintf(os.Stderr, "transport url ", url, "\n")
 	r, err := c.client.Post(url, "text/plain", nil)
 	if err != nil {
-		fmt.Fprintf(os.Stdout, "transport error ", err.Error(), "\n")
+		fmt.Fprintf(os.Stderr, "transport error ", err.Error(), "\n")
 		return err
 	}
 	if r.StatusCode != http.StatusOK {
-		fmt.Fprintf(os.Stdout, "transport status code: ", r.StatusCode, "\n")
+		fmt.Fprintf(os.Stderr, "transport status code: ", r.StatusCode, "\n")
 		return fmt.Errorf("SendMetric status %v", http.StatusOK)
 	}
 	r.Body.Close()
