33921957f153122b52986ebfc259f25a9cc337b4
artembychkov <mai.lhome@ya.ru>
2022-04-09 00:41:04 +0300

    add log and git ignor

--- internal/agent/transport/transport.go
+++ internal/agent/transport/transport.go
@@ -2,6 +2,7 @@ package transport
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/ChipArtem/Metric/internal/models"
@@ -20,12 +21,15 @@ func NewHTTPClient(url string, client *http.Client) HTTPclient {
 }
 
 func (c HTTPclient) SendMetric(m models.Metric) error {
+	client := &http.Client{}
 	url := c.hostURL + "/update/" + m.Type + "/" + m.Name + "/" + m.Value
-	r, err := c.client.Post(url, "text/plain", nil)
+	r, err := client.Post(url, "text/plain", nil)
 	if err != nil {
+		log.Printf("SendMetric error %v", http.StatusOK)
 		return err
 	}
 	if r.StatusCode != http.StatusOK {
+		log.Printf("SendMetric status %v", http.StatusOK)
 		return fmt.Errorf("SendMetric status %v", http.StatusOK)
 	}
 	r.Body.Close()
